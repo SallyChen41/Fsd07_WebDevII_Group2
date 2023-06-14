c823649b879ca33fd3f6c084694845d7ffba00dd
SiyiC41 <csyclara0122@gmail.com>
2023-06-14 09:54:02 -0400

    PayPal add

--- Project_O'ICI SUSHI/pages/cart.jsx
+++ Project_O'ICI SUSHI/pages/cart.jsx
@@ -1,12 +1,18 @@
-import React, { useContext } from 'react';
-import styles from '../styles/Cart.module.css';
-import Image from 'next/legacy/image';
-import { cartContext } from './cartContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import React, { useContext, useState, useEffect } from "react";
+import styles from "../styles/Cart.module.css";
+import Image from "next/legacy/image";
+import { cartContext } from "./cartContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
 
 const Cart = () => {
-  const { cart, updateItemQuantity, removeItemFromCart } = useContext(cartContext);
+  const { cart, updateItemQuantity, removeItemFromCart } =
+    useContext(cartContext);
 
   const total = cart.items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -20,6 +26,65 @@ const Cart = () => {
 
   const handleRemoveItem = (itemId) => {
     removeItemFromCart(itemId);
+  };
+
+  const [open, setOpen] = useState(false);
+  const [cash, setCash] = useState(false);
+  const amount = "2";
+  const currency = "USD";
+  const style = { layout: "vertical" };
+
+  const ButtonWrapper = ({ currency, showSpinner }) => {
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+    useEffect(() => {
+      dispatch({
+        type: "resetOptions",
+        value: {
+          ...options,
+          currency: currency,
+        },
+      });
+    }, [currency, showSpinner]);
+
+    return (
+      <>
+        {showSpinner && isPending && <div className="spinner" />}
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[amount, currency, style]}
+          fundingSource={undefined}
+          createOrder={(data, actions) => {
+            return actions.order
+              .create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: currency,
+                      value: amount,
+                    },
+                  },
+                ],
+              })
+              .then((orderId) => {
+                return orderId;
+              });
+          }}
+          onApprove={function (data, actions) {
+            return actions.order.capture().then(function (details) {
+              const shipping = details.purchase_units[0].shipping;
+              createOrder({
+                customer: shipping.name.full_name,
+                address: shipping.address.address_line_1,
+                total: cart.total,
+                method: 1,
+              });
+            });
+          }}
+        />
+      </>
+    );
   };
 
   return (
@@ -43,7 +108,9 @@ const Cart = () => {
                   <span className={styles.name}>{item.name}</span>
                 </td>
                 <td>
-                  <span className={styles.extras}>{item.extras.join(', ')}</span>
+                  <span className={styles.extras}>
+                    {item.extras.join(", ")}
+                  </span>
                 </td>
                 <td>
                   <span className={styles.price}>${item.price}</span>
@@ -63,7 +130,7 @@ const Cart = () => {
                   </span>
                 </td>
                 <td>
-                <button
+                  <button
                     className={styles.deleteButton}
                     onClick={() => handleRemoveItem(item.id)}
                   >
@@ -81,11 +148,35 @@ const Cart = () => {
           <div className={styles.totalText}>
             <b className={styles.totalTextTitle}>Total:</b>${total.toFixed(2)}
           </div>
-          <button className={styles.button}>CHECKOUT</button>
+          {open ? (
+            <div className={styles.paymentMethods}>
+              <button
+                className={styles.payButton}
+                onClick={() => setCash(true)}
+              >
+                CASH ON DELIVERY
+              </button>
+              <PayPalScriptProvider
+                options={{
+                  "client-id":
+                    "Af4eEHK9SLC7TlZ29e9AKCdwtxnk3--ZHZvF6cv--qrvdq5bYKYuxq9bsuVh3w13nTxKpFOLd_5SWlns",
+                  components: "buttons",
+                  currency: "USD",
+                  "disable-funding": "credit,card,p24",
+                }}
+              >
+                <ButtonWrapper currency={currency} showSpinner={false} />
+              </PayPalScriptProvider>
+            </div>
+          ) : (
+            <button onClick={() => setOpen(true)} className={styles.button}>
+              CHECKOUT NOW!
+            </button>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
