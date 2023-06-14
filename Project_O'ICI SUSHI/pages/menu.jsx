35ed7cb9e1140dc10383ae8031e0abfeb4c337b2
jean9947 <jean7499@gmail.com>
2023-06-14 12:19:17 -0400

    menu and item done

--- Project_O'ICI SUSHI/pages/menu.jsx
+++ Project_O'ICI SUSHI/pages/menu.jsx
@@ -1,47 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import styles from "../styles/Menu.module.css";
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Menu.module.css';
 import SushiCategory from '../components/SushiCategory';
 import SushiCard from '../components/SushiCard';
 import { collection, getDocs } from 'firebase/firestore';
 import { firestore } from '../config/firebase';
 
-const menu = () => {
-
+const Menu = () => {
   const [menuData, setMenuData] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    const fetchMenuData = async () => {
+    const fetchData = async () => {
       try {
-        const menuCollection = collection(firestore, 'menu');
-        const menuSnapshot = await getDocs(menuCollection);
-        const menuItems = menuSnapshot.docs.map((doc) => doc.data());
+        const itemsCollection = collection(firestore, 'items');
+        const categoriesCollection = collection(firestore, 'categories');
+    
+        const [itemsSnapshot, categoriesSnapshot] = await Promise.all([
+          getDocs(itemsCollection),
+          getDocs(categoriesCollection),
+        ]);
+    
+        const categoriesMap = categoriesSnapshot.docs.reduce((map, doc) => {
+          const category = doc.data();
+          map[category.name] = doc.id;
+          return map;
+        }, {});
+    
+        const itemsData = itemsSnapshot.docs.map((doc) => {
+          const itemData = doc.data();
+          const categoryId = categoriesMap[itemData.category];
+          if (categoryId === undefined) {
+            throw new Error(`Category ID not found for category: ${itemData.category}`);
+          }
+          return {
+            id: doc.id,
+            ...itemData,
+            category: itemData.category,
+          };
+        });
+    
+        const menuItems = categoriesSnapshot.docs.map((doc) => {
+          const category = doc.data();
+          const categoryId = doc.id;
+          const categoryItems = itemsData.filter(
+            (item) => item.category === category.name
+          );
+          return {
+            id: categoryId,
+            name: category.name,
+            items: categoryItems,
+          };
+        });
+    
         setMenuData(menuItems);
+        setCategories(categoriesSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
       } catch (error) {
-        console.log('Error fetching menu data:', error.message);
+        console.log('Error fetching items and categories:', error.message);
       }
     };
 
-    fetchMenuData();
+    fetchData();
   }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.menuContainer}>
         <div className={styles.menu}>
-          <SushiCategory></SushiCategory>
+          <SushiCategory categories={categories} />
         </div>
       </div>
-      {menuData.map((category) => (
-        <div key={category.categoryId}>
-          <h2>{category.name}</h2>
+      {menuData.map((item) => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
           <div className={styles.itemCategory}>
-            {category.items.map((item) => (
+            {item.items && item.items.map((menuItem) => (
               <SushiCard
-                key={item.itemId}
-                itemName={item.itemName}
-                description={item.description}
-                price={item.price}
-                image={item.image}
+                key={menuItem.id}
+                id={menuItem.id}
+                itemName={menuItem.itemName}
+                description={menuItem.description}
+                price={menuItem.price}
+                image={menuItem.image}
               />
             ))}
           </div>
@@ -51,4 +90,4 @@ const menu = () => {
   )
 }
 
-export default menu
+export default Menu;
